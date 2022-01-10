24d37fe2194dc6e2f266ce12fdfd58bf2eb58468
Lask LK <lhxlive@qq.com>
2022-01-10 21:38:19 +0800

    添加项目文件。

---
+++ main.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class main {
+public:
+	main(void) {
+		cout << "Hello, C艹. " << endl			\
+			<< "Hello, Visual Studio. " << endl	\
+			<< "Hello ReSharper!" << endl;
+	}
+};
